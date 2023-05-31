f9fa44d7daa033e2f80d91443a1bc7d918b36337
shangchian <shangchiann@gmail.com>
2023-05-31 13:19:20 +0800

    表單與驗證

--- MyModels/Book.cs
+++ MyModels/Book.cs
@@ -1,9 +1,14 @@
-﻿namespace MyModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyModels
 {
     public class Book
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(50, ErrorMessage = "{0} must be less than {1}")]
         public string Title { get; set; } = null!;  // 驚嘆號表示: 暫時為空值，初始化一定會有初值
+        [Range(10,500, ErrorMessage ="{0} must be between {1} - {2}")]
         public int Price { get; set; }
         public DateTime PublishDate { get; set; }
         public bool InStock { get; set; }
